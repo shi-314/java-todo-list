53fd13d50fe31a5498fe738a6fc7a39c6f250c57
Shivan Taher <shi.taher@gmail.com>
2024-01-05 18:14:26 +0100

    Initial commit

---
+++ src/main/java/com/github/shivan/javatodolist/JavaTodoListApplication.java
@@ -0,0 +1,13 @@
+package com.github.shivan.javatodolist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaTodoListApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaTodoListApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/github/shivan/javatodolist/JavaTodoListApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.shivan.javatodolist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaTodoListApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
