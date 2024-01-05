668e7ed82a787a116a41caddc30b6a72b529d939
Shivan Taher <shi.taher@gmail.com>
2024-01-05 19:43:22 +0100

    Add request params

--- src/main/java/com/github/shivan/javatodolist/controller/TodoListController.java
+++ src/main/java/com/github/shivan/javatodolist/controller/TodoListController.java
@@ -1,6 +1,7 @@
 package com.github.shivan.javatodolist.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -8,10 +9,13 @@ public class TodoListController {
 
     @GetMapping("/todo-list")
     public String getTodoList() {
-        return "Todo List";
+        return "<b>Todo</b> List";
     }
-    @GetMapping ("Add-Item")
-    public String addItem() {
-        return "Add Item";
+
+    @GetMapping("/add-item")
+    public String addItem(
+        @RequestParam("item") String item
+    ) {
+        return "Add Item: " +  item;
     }
 }
