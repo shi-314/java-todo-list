a6290da746192d5d07c02202fad2d8189c2f1158
Shivan Taher <shi.taher@gmail.com>
2024-01-05 21:46:57 +0100

    Add basic todoList store in-memory

--- src/main/java/com/github/shivan/javatodolist/controller/TodoListController.java
+++ src/main/java/com/github/shivan/javatodolist/controller/TodoListController.java
@@ -4,18 +4,30 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
+
 @RestController
 public class TodoListController {
+    ArrayList<String> todoList = new ArrayList<String>();
 
     @GetMapping("/todo-list")
     public String getTodoList() {
-        return "<b>Todo</b> List";
+
+        String todoListString = "";
+
+        for (String item : todoList) {
+            todoListString += item + "<br>";
+        }
+
+        return todoListString;
     }
 
     @GetMapping("/add-item")
     public String addItem(
         @RequestParam("item") String item
     ) {
+        todoList.add(item);
+
         return "Add Item: " +  item;
     }
 }
